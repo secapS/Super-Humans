ae687d0199c225ee5ce689853d5ca2a9bbd38184
Luke <spaceslulzz@gmail.com>
2016-08-29 10:30:18 -1000

    Made SuperHumanTask abstract.

--- src/main/java/xyz/whynospaces/superhumans/api/SuperHumanTask.java
+++ src/main/java/xyz/whynospaces/superhumans/api/SuperHumanTask.java
@@ -3,7 +3,7 @@ package xyz.whynospaces.superhumans.api;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
-public class SuperHumanTask extends BukkitRunnable {
+public abstract class SuperHumanTask extends BukkitRunnable {
 
     private Player player;
     private SuperHuman superHuman;
@@ -11,11 +11,6 @@ public class SuperHumanTask extends BukkitRunnable {
     public SuperHumanTask(Player player, SuperHuman superHuman) {
         this.player = player;
         this.superHuman = superHuman;
-    }
-
-    @Override
-    public void run() {
-
     }
 
     public Player getPlayer() {
